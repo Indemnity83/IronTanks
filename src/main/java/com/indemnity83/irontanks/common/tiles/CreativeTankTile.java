ccbce352c5e78987911a30928abc39be86ced5fc
Kyle Klaus <kklaus@indemnity83.com>
2018-02-12 10:24:53 -0800

    disable logical stacking of creative tank

--- src/main/java/com/indemnity83/irontanks/common/tiles/CreativeTankTile.java
+++ src/main/java/com/indemnity83/irontanks/common/tiles/CreativeTankTile.java
@@ -1,11 +1,16 @@
 package com.indemnity83.irontanks.common.tiles;
 
 import buildcraft.api.core.IFluidFilter;
+import buildcraft.factory.tile.TileTank;
 import net.minecraftforge.fluids.FluidStack;
 
 public class CreativeTankTile extends TankTile {
     @Override
     public FluidStack drain(IFluidFilter filter, int maxDrain, boolean doDrain) {
         return super.drain(filter, maxDrain, false);
+    }
+
+    public boolean canConnectTo(TileTank tileTank) {
+        return false;
     }
 }
